7212a5bdf160fb4af15c604a5eb5be3b433b9f3c
Creektus <5347100@gmail.com>
2022-11-29 03:16:52 +0300

    add Method Array

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -1,3 +1,18 @@
 ﻿string[] array1 = { "KeK", "Hi!", "Moscow",
       "Go", "Denmark", "Friday", "SOS" };
 string[] array2 = new string[array1.Length];
+
+void methodArray (string [] array1)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {      
+        if (array1[i].Length <= 3)
+        {
+        array2 [count]=array1[i];;
+        count++;
+        }
+    }
+}
+
+methodArray(array1);
